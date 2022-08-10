dbd49a28e09e4be09c52098c712acd6e5c9cc465
Giovana Carvalho <giovanaaraujo3003@gmail.com>
2022-08-10 16:20:02 -0300

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import { Logger } from "./main/logger";
+import { Controller } from "./protocols/controller";
+import { IDataLogger } from "./protocols/logger";
 
-export default new Logger()
+const logger = new Logger();
+
+export * from "./protocols/controller"
+export * from "./protocols/logger"
+export const logController = (data: IDataLogger, controller: Controller) => logger.log(data, controller)
+export const logMiddleware = (data: IDataLogger) => logger.log(data)
